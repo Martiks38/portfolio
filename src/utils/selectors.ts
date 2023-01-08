55e4ce4b90ca3c26b6665e4087363f7e762367ce
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-01-08 01:16:15 -0300

    Crear manejador del botón menú

---
+++ src/utils/selectors.ts
@@ -0,0 +1,5 @@
+const $ = (selector: string) => document.querySelector<HTMLElement>(selector)
+const $$ = (selector: string) =>
+  document.querySelectorAll<HTMLElement>(selector)
+
+export { $, $$ }
