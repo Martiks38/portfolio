5e082bdd36b62ddd1d331b328b43a373c48bfe11
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-25 20:47:27 -0300

    Añadir tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,17 @@
 module.exports = {
-  extends: [
-    "plugin:astro/recommended"
-  ],
-  overrides: [
-    {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-      rules: {
-        "space-before-function-paren":["error", "never"]      
-      },
-    },
-  ],
+	extends: ['plugin:astro/recommended', 'plugin:@typescript-eslint/recommended'],
+	rules: {
+		'space-before-function-paren': ['error', 'never']
+	},
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro']
+			},
+			rules: {}
+		}
+	]
 }
