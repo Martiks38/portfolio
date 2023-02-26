13ec3b5cb8ed169e301db4b6c9e7e7ccfa6794c6
Giuliano Cappa <cappa.giuliano@gmail.com>
2023-02-26 20:00:25 -0300

    Modificar subtítulo y la sección proyectos

--- src/utils/encrypted.js
+++ src/utils/encrypted.js
@@ -4,99 +4,99 @@
  * Genera letras alfanuméricas aleatoriamente hasta alcanzar la cantidad de iteraciones indicada.
  */
 class EffectDecryptingText {
-  word = 'Desarrollador Front End React'
+	word = 'Desarrollador Front End'
 
-  constructor(selector, stagger = 100, staggerPerLetter = 50) {
-    this.element = document.querySelector(selector)
-    this.letters = this.word.split('')
-    this.stagger = stagger
-    this.staggerPerLetter = staggerPerLetter
+	constructor(selector, stagger = 100, staggerPerLetter = 50) {
+		this.element = document.querySelector(selector)
+		this.letters = this.word.split('')
+		this.stagger = stagger
+		this.staggerPerLetter = staggerPerLetter
 
-    this.animate()
-  }
+		this.animate()
+	}
 
-  /**
-   * Start the decryption animation.
-   * Creates the container for each letter and inserts them.
-   */
-  animate() {
-    const encryptedText = this.element.innerText
-    const fragment = document.createDocumentFragment()
+	/**
+	 * Start the decryption animation.
+	 * Creates the container for each letter and inserts them.
+	 */
+	animate() {
+		const encryptedText = this.element.innerText
+		const fragment = document.createDocumentFragment()
 
-    const lettersEncryptedText = encryptedText.split('')
-    this.element.innerText = ''
+		const lettersEncryptedText = encryptedText.split('')
+		this.element.innerText = ''
 
-    this.letters.forEach((letter, index) => {
-      const span = document.createElement('span')
+		this.letters.forEach((letter, index) => {
+			const span = document.createElement('span')
 
-      span.innerText = lettersEncryptedText.at(index)
+			span.innerText = lettersEncryptedText.at(index)
 
-      fragment.appendChild(span)
+			fragment.appendChild(span)
 
-      setTimeout(() => {
-        this.decryptLetter({ elementLetter: span, letter })
-      }, this.stagger)
-    })
+			setTimeout(() => {
+				this.decryptLetter({ elementLetter: span, letter })
+			}, this.stagger)
+		})
 
-    this.element.appendChild(fragment)
-  }
+		this.element.appendChild(fragment)
+	}
 
-  /**
-   * Assign random characters until the desired number of iterations is reached.
-   * Once the number of iterations is reached, it changes the color style and assigns the correct letter.
-   *
-   * @param {object} options
-   * @property {string} options.letter - Letter of the correct word.
-   * @property {HTMLElement} options.elementLetter - One letter container.
-   * @param {number} count - Number of iterations.
-   * @returns
-   */
-  decryptLetter(options, count = 0) {
-    if (count > 10) {
-      const subtitle = document.querySelector('.introduction-subtitle')
+	/**
+	 * Assign random characters until the desired number of iterations is reached.
+	 * Once the number of iterations is reached, it changes the color style and assigns the correct letter.
+	 *
+	 * @param {object} options
+	 * @property {string} options.letter - Letter of the correct word.
+	 * @property {HTMLElement} options.elementLetter - One letter container.
+	 * @param {number} count - Number of iterations.
+	 * @returns
+	 */
+	decryptLetter(options, count = 0) {
+		if (count > 10) {
+			const subtitle = document.querySelector('.introduction-subtitle')
 
-      subtitle.style.color = '#75eff8'
-      options.elementLetter.innerText = options.letter
+			subtitle.style.color = '#75eff8'
+			options.elementLetter.innerText = options.letter
 
-      return
-    }
+			return
+		}
 
-    count++
+		count++
 
-    setTimeout(() => {
-      options.elementLetter.innerText = this.generateRandomLetter()
+		setTimeout(() => {
+			options.elementLetter.innerText = this.generateRandomLetter()
 
-      this.decryptLetter(options, count)
-    }, this.staggerPerLetter)
-  }
+			this.decryptLetter(options, count)
+		}, this.staggerPerLetter)
+	}
 
-  /**
-   * Generates a string of random characters whose length is equal to the true string.
-   */
-  encryptText() {
-    let encryptedText = ''
+	/**
+	 * Generates a string of random characters whose length is equal to the true string.
+	 */
+	encryptText() {
+		let encryptedText = ''
 
-    this.letters.forEach(() => {
-      const randomLetter = this.generateRandomLetter()
+		this.letters.forEach(() => {
+			const randomLetter = this.generateRandomLetter()
 
-      encryptedText += randomLetter
-    })
+			encryptedText += randomLetter
+		})
 
-    return encryptedText
-  }
+		return encryptedText
+	}
 
-  /**
-   * Returns the third character of the number returned by Math.random()
-   */
-  generateRandomLetter() {
-    return Math.random().toString(36).substring(2, 3)
-  }
+	/**
+	 * Returns the third character of the number returned by Math.random()
+	 */
+	generateRandomLetter() {
+		return Math.random().toString(36).substring(2, 3)
+	}
 }
 
 export const startDecryptingText = (stagger = 1000) => {
-  const selectors = ['.introduction-subtitle']
+	const selectors = ['.introduction-subtitle']
 
-  setTimeout(() => {
-    selectors.forEach((selector) => new EffectDecryptingText(selector))
-  }, stagger)
+	setTimeout(() => {
+		selectors.forEach((selector) => new EffectDecryptingText(selector))
+	}, stagger)
 }
