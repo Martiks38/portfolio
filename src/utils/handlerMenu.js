b87255d003b44d0d27ddc308fb5452a2d2a6f0a7
Martiks38 <cappa.giuliano@gmail.com>
2023-06-11 16:57:57 -0300

    refactor: hacer que handlerMenu maneje un solo botón

--- src/utils/handlerMenu.js
+++ src/utils/handlerMenu.js
@@ -1,17 +1,14 @@
 import { $, $$ } from './selectors'
 
 const header = $('.headerPage')
-const menuBtns = $$('.menuBtn')
+const menuBtn = $('.menuBtn')
 const itemsMenu = $$('.navMenu__item')
 
 /** Cambie los atributos de aria y modifique el botón de menú y muestre u oculte la lista de menú */
 const changeMenuView = () => {
-	const visibleMenu = menuBtns[0].classList.contains('collapse')
-
-	menuBtns.forEach((menuBtn) => {
-		menuBtn.setAttribute('aria-expanded', visibleMenu ? 'false' : 'true')
-		menuBtn.classList.toggle('collapse')
-	})
+	const visibleMenu = menuBtn.classList.contains('collapse')
+	menuBtn.setAttribute('aria-expanded', visibleMenu ? 'false' : 'true')
+	menuBtn.classList.toggle('collapse')
 
 	header.setAttribute('aria-hidden', visibleMenu ? 'true' : 'false')
 	header.classList.toggle('menu-view')
@@ -20,13 +17,13 @@ const changeMenuView = () => {
 /** Al elegir una de las secciones de la página, ejecuta la función changeMenuView */
 itemsMenu.forEach((itemMenu) => itemMenu.addEventListener('click', changeMenuView))
 
-menuBtns.forEach((menuBtn) => menuBtn.addEventListener('click', changeMenuView))
+menuBtn.addEventListener('click', changeMenuView)
 
 /** Llama a la función changeMenuView() al hacer clic fuera del menú de navegación o del botón de menú */
 document.addEventListener('click', (e) => {
 	const target = e.target
 	const isHeaderOrMenu = target.closest('.headerPage') || target.closest('.menuBtn')
-	const isCollapse = menuBtns[0].classList.contains('collapse')
+	const isCollapse = menuBtn.classList.contains('collapse')
 
 	if (!isHeaderOrMenu && isCollapse) changeMenuView()
 })
@@ -35,10 +32,10 @@ document.addEventListener('click', (e) => {
 document.addEventListener('DOMContentLoaded', () => {
 	const width = window.innerWidth
 
-	if (width < 776) {
-		menuBtns.forEach((menuBtn) => menuBtn.setAttribute('aria-hidden', 'false'))
-		header.setAttribute('aria-hidden', 'true')
-	}
+	if (width > 776) return
+
+	menuBtn.setAttribute('aria-hidden', 'false')
+	header.setAttribute('aria-hidden', 'true')
 })
 
 /** Modifies the aria attributes according to the new dimensions */
@@ -46,12 +43,12 @@ window.addEventListener('resize', () => {
 	const width = window.innerWidth
 
 	if (width < 776) {
-		menuBtns.forEach((menuBtn) => menuBtn.setAttribute('aria-hidden', 'false'))
+		menuBtn.setAttribute('aria-hidden', 'false')
 		header.setAttribute('aria-hidden', 'true')
 		return
 	}
 
 	header.removeAttribute('aria-hidden')
 	header.classList.remove('menu-view')
-	menuBtns.forEach((menuBtn) => menuBtn.classList.remove('collapse'))
+	menuBtn.classList.remove('collapse')
 })
