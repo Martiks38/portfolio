e59289d07fd7d95082b2ff4adba402f24b155d52
Martiks38 <cappa.giuliano@gmail.com>
2023-08-07 10:23:33 -0300

    refactor: añadir configuración de configurar cssnano

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,12 @@
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const purgecss = require('@fullhuman/postcss-purgecss')
+const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
 
 module.exports = {
 	plugins: [
 		require('postcss-nested'),
-		require('autoprefixer'),
-		require('cssnano'),
+		cssnano({ plugins: [autoprefixer], preset: 'default' }),
 		purgecss({
 			content: ['src/**/*.astro'],
 			css: ['src/**/*.css'],
