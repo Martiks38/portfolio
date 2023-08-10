190148296ec8b95042b667b9b9bc8e0585142ff9
Martiks38 <cappa.giuliano@gmail.com>
2023-08-10 11:04:50 -0300

    feat: añadir internalización

--- src/consts.ts
+++ src/consts.ts
@@ -3,13 +3,6 @@ const COLOR_ASPECT = {
 	light: 'claro'
 }
 
-const ITEMS_NAV = {
-	mainContent: 'INICIO',
-	'about-me': 'SOBRE MÍ',
-	projects: 'PROYECTOS',
-	contact: 'CONTACTO'
-}
-
 const BASE_URL_GITHUB = 'https://github.com/Martiks38/'
 
 const NAME_COOKIE_COLOR = 'prefersColor'
@@ -17,4 +10,4 @@ const NAME_COOKIE_COLOR = 'prefersColor'
 // sixty days
 const MAX_AGE_COOKIE = 24 * 60 * 60 * 60
 
-export { BASE_URL_GITHUB, COLOR_ASPECT, ITEMS_NAV, NAME_COOKIE_COLOR, MAX_AGE_COOKIE }
+export { BASE_URL_GITHUB, COLOR_ASPECT, NAME_COOKIE_COLOR, MAX_AGE_COOKIE }
---
+++ src/values.ts
@@ -0,0 +1,8 @@
+import i18n from '@assets/translations/i18n.json'
+
+export const ITEMS_NAV = {
+	mainContent: i18n.es.NAVIGATION_MENU.home,
+	projects: i18n.es.NAVIGATION_MENU.projects,
+	'about-me': i18n.es.NAVIGATION_MENU['about-me'],
+	contact: i18n.es.NAVIGATION_MENU.contact
+}
