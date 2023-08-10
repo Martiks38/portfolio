cc5ac074506ad1cb29df0af039767c9609094d5e
Martiks38 <cappa.giuliano@gmail.com>
2023-08-10 14:25:02 -0300

    refactor: reordenar y mejorar el código

--- src/utils/changeTheme.ts
+++ src/utils/changeTheme.ts
@@ -1,8 +1,20 @@
+import i18n from '@assets/translations/i18n.json'
 import { COLOR_ASPECT, MAX_AGE_COOKIE, NAME_COOKIE_COLOR } from '@/consts'
 
 const { dark, light } = COLOR_ASPECT
 const btnsPrefersColor = document.querySelectorAll<HTMLButtonElement>('.btnColorScheme')
-const btnPrefersColor = btnsPrefersColor[0] ?? null
+
+/**
+ *	Busca si existe un estado del modo de pantalla por previo ingreso a la página. En el caso que sea el primer ingreso, establece una cookie con el tema de página en modo oscuro y a los botones data-color con el mismo modo.
+ */
+const cookieStorage = window.document.cookie
+const hasNotPrefersColor = !cookieStorage.includes(NAME_COOKIE_COLOR)
+
+if (hasNotPrefersColor) {
+	btnsPrefersColor.forEach((btnPrefersColor) => btnPrefersColor.setAttribute('data-color', dark))
+
+	window.document.cookie = `${NAME_COOKIE_COLOR}=${dark};max-age=${MAX_AGE_COOKIE}`
+}
 
 /**
  * Dado que hacer toggle de una clase que rote el ícono del sol o la luna hará efecto de que vuelva a la posición.
@@ -16,43 +28,46 @@ const addTurns = () => {
 
 const nextTurn = addTurns()
 
-const changeColorScheme = () => {
-	if (btnPrefersColor === null) return
+const changeColorScheme = (currentColor: string, turns: string) => {
+	const currentColorIsDark = currentColor === dark
 
-	const currentColorIsDark = btnPrefersColor?.dataset.color === dark
+	const newCurrentColor = currentColorIsDark ? light : dark
+	const newChangeColor = currentColorIsDark ? dark : light
 
-	const currentColor = currentColorIsDark ? light : dark
-	const changeColor = currentColorIsDark ? dark : light
+	const prevTurn = Number(turns)
+	const bodyClassList = document.body.classList
 
-	const prevTurn = Number(btnPrefersColor.dataset.turns)
-
-	if (currentColorIsDark) {
-		document.body.classList.add('light')
-	} else {
-		document.body.classList.remove('light')
-	}
+	currentColorIsDark ? bodyClassList.add('light') : bodyClassList.remove('light')
 
 	const currentTurn = nextTurn()
 	const totalTurn = currentTurn + prevTurn
 
 	btnsPrefersColor.forEach((btnPrefersColor) => {
 		btnPrefersColor.style.transform = `rotate(${totalTurn * 0.5}turn)`
-		btnPrefersColor.setAttribute('data-color', currentColor)
-		btnPrefersColor.setAttribute('aria-label', `Cambiar aspecto a modo ${changeColor}`)
+		btnPrefersColor.setAttribute('data-color', newCurrentColor)
+		btnPrefersColor.setAttribute(
+			'aria-label',
+			`${i18n.es.BUTTON_CHANGE_PAGE_SCHEME} ${newChangeColor}`
+		)
 	})
 
-	document.cookie = `${NAME_COOKIE_COLOR}=${currentColor};max-age=${MAX_AGE_COOKIE}`
+	document.cookie = `${NAME_COOKIE_COLOR}=${newCurrentColor};max-age=${MAX_AGE_COOKIE}`
 }
 
-const cookieStorage = window.document.cookie
-const hasNotPrefersColor = !cookieStorage.includes(NAME_COOKIE_COLOR)
-
-if (hasNotPrefersColor) {
-	btnsPrefersColor.forEach((btnPrefersColor) => btnPrefersColor?.setAttribute('data-color', dark))
-
-	window.document.cookie = `${NAME_COOKIE_COLOR}=${dark};max-age=${MAX_AGE_COOKIE}`
-}
-
+// Añade la acción de cambiar el aspecto de la página a los respectivos botones
 btnsPrefersColor.forEach((btnPrefersColor) =>
-	btnPrefersColor.addEventListener('click', changeColorScheme, false)
+	btnPrefersColor.addEventListener(
+		'click',
+		(ev) => {
+			const btn = ev.currentTarget
+
+			if (btn instanceof HTMLButtonElement) {
+				const currentColor = btn.dataset.color
+				const turns = btn.dataset.turns
+
+				if (currentColor && turns) changeColorScheme(currentColor, turns)
+			}
+		},
+		false
+	)
 )
