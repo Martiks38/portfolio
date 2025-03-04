eeb58e8ac08f73cc511f75c3f31a50688402e247
Martiks <cappa.giuliano@gmail.com>
2025-03-04 16:25:00 -0300

    remover funcionalidad de cambio de tema y añadir dependencia

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,3 @@
-const COLOR_ASPECT = {
-	dark: 'oscuro',
-	light: 'claro'
-}
-
 const EXTERNAL_LINKS = {
 	midudev: 'https://www.youtube.com/@midulive',
 	jonmircha: 'https://www.youtube.com/@jonmircha'
@@ -10,6 +5,5 @@ const EXTERNAL_LINKS = {
 
 const BASE_URL_GITHUB = 'https://github.com/Martiks38'
 
-const COLOR_SCHEME = 'prefersColor'
 
-export { BASE_URL_GITHUB, COLOR_ASPECT, EXTERNAL_LINKS, COLOR_SCHEME }
+export { BASE_URL_GITHUB, EXTERNAL_LINKS }
