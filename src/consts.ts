642220a3f74306ae4bf834f05bf6aa90a74eeda6
Martiks <cappa.giuliano@gmail.com>
2025-03-09 16:38:54 -0300

    modificar sección about me

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,4 @@
-const EXTERNAL_LINKS = {
-	midudev: 'https://www.youtube.com/@midulive',
-	jonmircha: 'https://www.youtube.com/@jonmircha'
-}
-
 const BASE_URL_GITHUB = 'https://github.com/Martiks38'
 
 
-export { BASE_URL_GITHUB, EXTERNAL_LINKS }
+export { BASE_URL_GITHUB }
